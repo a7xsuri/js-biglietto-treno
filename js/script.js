ac2e355d027854d0e579c591fa03f81c7bb93058
a7xsuri <leonardofilipposurico@gmail.com>
2022-03-24 14:47:47 +0100

    creata variabile prezzo km età e risultato + molt.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const prezzo = 0.21;
+
+let km = prompt('quanti chilometri vuoi percorrere?');
+console.log(km);
+
+let età = prompt('quanti anni hai?');
+console.log(età);
+
+let risultato;
+
+risultato = prezzo * km;
