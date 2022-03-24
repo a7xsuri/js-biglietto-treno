5800fc0584c3b90539c8db631da5df189a03ff76
a7xsuri <leonardofilipposurico@gmail.com>
2022-03-24 15:14:43 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -4,8 +4,22 @@ let km = prompt('quanti chilometri vuoi percorrere?');
 console.log(km);
 
 let età = prompt('quanti anni hai?');
-console.log(età);
 
 let risultato;
 
 risultato = prezzo * km;
+
+if ( età < 18 ) {
+    risultato = risultato - (risultato * 0.2);
+    document.getElementById('ris').innerHTML = 'Il prezzo del biglietto è: ' + (Math.round(risultato * 100) / 100 ) + '€';
+    console.log(risultato);
+}
+else if ( età>=65 ) {
+    risultato = risultato - (risultato * 0.4);
+    document.getElementById('ris').innerHTML = 'Il prezzo del biglietto è: ' + (Math.round(risultato * 100) / 100 ) + '€';
+    console.log(risultato);
+}
+else {
+    document.getElementById('ris').innerHTML = 'Il prezzo del biglietto è: ' + risultato + '€';
+    console.log(risultato)
+}
